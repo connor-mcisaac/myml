1e833cb2a8a3a0a537930fa07a50e763462054f9
connor-mcisaac <connor.mcisaac@outlook.com>
2018-11-28 22:58:03 +0000

    sorted mnist data

--- myml.py
+++ myml.py
@@ -1,20 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
 
 
 class mynet(object):
+
+	def linear(x):
+		return x
 	
-	def __init__(self, ninput, noutput, activation='linear'):
+	def __init__(self, ninput, noutput, activation=linear):
 		self.nlayers = 1
 		self.ninput = ninput
 		self.layers = [noutput]
 		self.weights = [np.random.randn(ninput, noutput)]
 		self.biases = [np.random.randn(noutput)]
+		self.activation = activation
 
 	def add_layer(self, nnodes, position=None):
-		if position is None:
-			position = self.nlayers-1
-		if position < 0:
-			position = self.nlayers + position
+		position = self.nlayers-1 if position is None else position
+		position = self.nlayers + position if position < 0 else position
 		self.nlayers += 1
 		self.layers.insert(position, nnodes)
 		self.weights.insert(position, np.random.randn(self.layers[position-1] if position > 0 else self.ninput, nnodes))
@@ -35,11 +39,20 @@ class mynet(object):
 			current = np.copy(ineval)
 			for i in range(self.nlayers):
 				print(np.shape(current))
-				current = np.matmul(current, self.weights[i]) + self.biases[i]
+				current = self.activation(np.matmul(current, self.weights[i]) + self.biases[i])
 			return current
 
 
-test = mynet(8, 3)
+def lincut(x):
+	return x.clip(min=0)
+
+
+def sigmoid(x):
+	return 1/(1 + np.exp(-x))
+
+
+np.random.seed(0)
+test = mynet(8, 3, sigmoid)
 test.print_layers()
 test.add_layer(5, position=0)
 test.print_layers()
@@ -51,3 +64,8 @@ test.add_layer(9, position=2)
 test.print_layers()
 out = test.evaluate(np.random.randn(8))
 print(out)
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train.reshape(len(x_train[:, 0]), -1)
+x_test = x_test.reshape(len(x_test[:, 0]), -1)
